62dc4136749aa2edbeb4ef34d645d52956856f9a
Kweaver31 <kweaver31@gmail.com>
2020-07-03 02:34:49 -0400

    finished python program builds HTML pages now

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-  va-webscraper is a program that will scan https://www.va.gov/ get all necessary information about educational and disability benefits
+  va-webscraper is a program that will scan https://www.va.gov/ get all necessary information about Veteran benefits
   The program will not run without installing the requests, and beautifulsoup4 libraries
   Assuming you have Python3 and pip3 installed both libraries can be installed by running the command 'install.sh' from the va-webscraper current directory
   To start va-webscraper, open terminal and run the command 'python3 va-webscraper.py' from the va-webscraper current directory
--- va-webscraper.py
+++ va-webscraper.py
@@ -1,22 +1,30 @@
-import requests #Import python requests library
-from bs4 import BeautifulSoup #Create Beatiful Soup object that takes scraped HTML content as input
+import requests #Import python requests Library
+from bs4 import BeautifulSoup #Import BeautifulSoup Library 
 
-print("va-webscraper has started\n") #Statement to let user know the program has started
+print("va-webscraper has started") #Statement to let user know the program has started
 
-my_url = 'https://www.va.gov/disability' #GET DISABILITY URL
+my_url = 'https://www.va.gov/' #Set variable my_url to va.gov website
 page = requests.get(my_url) #Perform HTTP request
 soup = BeautifulSoup(page.content, 'html.parser') #Parse file and call it soup
 
-links_section = soup.findAll(id_='homepage-benefits') #Get info from HTML id
-"""
-#Opens connection to VA website through Function uReq
-uVA = uReq(my_url)
-#offloads info into a variable and closes the client
-page_html = uVA.read()
-uVA.close()
-#html parsing
-page_soup = soup(page_html, "html.parser")
+links_section = soup.find(id='homepage-benefits') #Get content from benefits section
+links_section = str(links_section) #Turn the links_section into a string
+links_section = links_section.replace("href=\"", "href=\"https://va.gov") #Route the href tags to va.gov
 
-claim = page_soup.find(id"72dea966a4245dd56a892f22b70f6c8a").text.strip()
-    print(claim)
-"""
+#Content for HTML page
+htmlpage = """<!DOCTYPE html>
+<html>
+	<head>
+		<title>VA-Scraper Page</title>
+	</head>
+	<body>
+""" + links_section + """
+	</body>
+</html>
+""" 
+
+f = open("index.html", "w") #Create or Open up a file with the name index.html and write to it
+f.write(htmlpage) #Write the content to the html page
+f.close() #Close file after writing
+
+print("Website has been generated with all helpful links for veterans seeking benefits and health care")
